5c112c13c03ea0a69affb354f136bf687ccd0657
Zachary Sweger <zsweger@berkeley.edu>
2024-09-30 22:58:43 -0700

    Update submitPileupScan.sh

--- calculateCumulants/submitPileupScan.sh
+++ calculateCumulants/submitPileupScan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of pileup rates
-PILEUP_RATES=("0.00001" "0.00001" "0.00002" "0.00005" "0.0001" "0.0002" "0.0005" "0.001" "0.002" "0.005" "0.01")
+PILEUP_RATES=("0.00001" "0.00002" "0.00005" "0.0001" "0.0002" "0.0005" "0.001" "0.002" "0.005" "0.01")
 
 # Loop over each pileup rate
 for PILEUP_RATE in "${PILEUP_RATES[@]}"
